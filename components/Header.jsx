19f0044f516f35b07a30fbe6f0980dd900b9ea4f
Thomas Zhang <zhj930924@gmail.com>
2021-12-07 10:22:28 -0800

    Added nav bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,28 +1,77 @@
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./Header.module.scss";
 
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
 const Header = () => {
+  const settings = ["Profile", "Logout"];
+  const pages = ["Survey", "Results", "Favorite"];
+  const [loggedIn, setLoggedIn] = useState(false);
+
   const router = useRouter();
   return (
     <header className={`${styles.header}`}>
-      {!["/", "/results"].includes(router.pathname) && (
-        <Image
-          className="back"
-          alt="back button"
-          src="/img/back.png"
-          layout="fixed"
-          width={44}
-          height={44}
-          onClick={() => router.back()}
-        />
-      )}
-      {router.pathname == "/results" && (
-        <Link href="/">
-          <a className={styles.logo}>reRoot</a>
-        </Link>
-      )}
+      <Navbar collapseOnSelect expand="lg" bg="transparent">
+        <Container>
+          <Navbar.Brand>
+            {["/survey"].includes(router.pathname) && (
+              <Image
+                className="back"
+                alt="back button"
+                src="/img/back.png"
+                layout="fixed"
+                width={44}
+                height={44}
+                onClick={() => router.back()}
+              />
+            )}
+            {["/", "/results", "/favorite"].includes(router.pathname) && (
+              <Link href="/">
+                <a className={styles.logo}>reRoot</a>
+              </Link>
+            )}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav>
+              {["/results"].includes(router.pathname) && (
+                <Nav.Link>
+                  <Link href="/favorite">
+                    <a className={`${styles.link}`}>FAVORITE</a>
+                  </Link>
+                </Nav.Link>
+              )}
+              {["/favorite"].includes(router.pathname) && (
+                <Nav.Link>
+                  <Link href="/results">
+                    <a className={`${styles.link}`}>RESULTS</a>
+                  </Link>
+                </Nav.Link>
+              )}
+              {loggedIn ? (
+                <NavDropdown
+                  title="User"
+                  id="collasible-nav-dropdown"
+                  className={`${styles.link}`}
+                >
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <Nav.Link>
+                  <a className={`${styles.link}`}>REGISTER / SIGN IN</a>
+                </Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };
---
+++ components/topNav/index.js
@@ -0,0 +1,176 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Header.module.scss";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+
+export default function TopNav(props) {
+  const settings = ["Profile", "Logout"];
+  const pages = ["Survey", "Results", "Favorite"];
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  const router = useRouter();
+  return (
+    <header className={`${styles.header}`}>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+            >
+              {["/survey"].includes(router.pathname) && (
+                <Image
+                  className="back"
+                  alt="back button"
+                  src="/img/back.png"
+                  layout="fixed"
+                  width={44}
+                  height={44}
+                  onClick={() => router.back()}
+                />
+              )}
+              {["/", "/results", "/favorite"].includes(router.pathname) && (
+                <Link href="/">
+                  <a className={styles.logo}>reRoot</a>
+                </Link>
+              )}
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+            >
+              {["/survey"].includes(router.pathname) && (
+                <Image
+                  className="back"
+                  alt="back button"
+                  src="/img/back.png"
+                  layout="fixed"
+                  width={44}
+                  height={44}
+                  onClick={() => router.back()}
+                />
+              )}
+              {["/", "/results", "/favorite"].includes(router.pathname) && (
+                <Link href="/">
+                  <a className={styles.logo}>reRoot</a>
+                </Link>
+              )}
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="User" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </header>
+  );
+}
