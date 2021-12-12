c45f71b0bdc4a9469806e51e71a4e2284408c715
Thomas Zhang <zhj930924@gmail.com>
2021-12-11 22:48:29 -0800

    seeded all parameters

--- data/categories.ts
+++ data/categories.ts
@@ -1,25 +1,31 @@
 export const categories = [
   {
+    id: 1,
     name: "affordability",
     text: "Affordability",
   },
   {
+    id: 2,
     name: "opportunity",
     text: "Opportunity",
   },
   {
+    id: 3,
     name: "community",
     text: "Community",
   },
   {
+    id: 4,
     name: "environment",
     text: "Environment",
   },
   {
+    id: 5,
     name: "vote",
     text: "Voting Power",
   },
   {
+    id: 6,
     name: "tax",
     text: "Tax Priority",
   },
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,42 @@
 import { parameters } from "../data/parameters";
 import { factors } from "../data/factors";
 import { categories } from "../data/categories";
+import { languages } from "../data/languages";
+import { countries } from "../data/countries";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.category.createMany({
-    data: categories,
-    skipDuplicates: true, // Skip 'Bobo'
-  });
-
+  // await prisma.category.createMany({
+  //   data: categories,
+  //   skipDuplicates: true, // Skip 'Bobo'
+  // });
+  // const allCategories = await prisma.category.findMany();
+  // console.dir(allCategories, { depth: null });
   // await prisma.factor.createMany({
   //   data: factors,
   //   skipDuplicates: true, // Skip 'Bobo'
   // });
-
   // const allFactors = await prisma.factor.findMany();
   // console.dir(allFactors, { depth: null });
-
-  const allCategories = await prisma.category.findMany();
-  console.dir(allCategories, { depth: null });
+  // await prisma.parameter.createMany({
+  //   data: parameters,
+  //   skipDuplicates: true, // Skip 'Bobo'
+  // });
+  // const allParameters = await prisma.parameter.findMany();
+  // console.dir(allParameters, { depth: null });
+  // await prisma.language.createMany({
+  //   data: languages,
+  //   skipDuplicates: true, // Skip 'Bobo'
+  // });
+  // const allLanguages = await prisma.language.findMany();
+  // console.dir(allLanguages, { depth: null });
+  // await prisma.country.createMany({
+  //   data: countries,
+  //   skipDuplicates: true, // Skip 'Bobo'
+  // });
+  // const allCountries = await prisma.country.findMany();
+  // console.dir(allCountries, { depth: null });
 }
 
 main()
